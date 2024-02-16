829cc895f9543af3d3a056f53b3b4b7969930534
AlanPrates <alanprates@MacBook-Pro-de-AlanPrates.local>
2024-02-15 18:03:02 -0800

    Implementar a funcionalidade de minimizar o aplicativo para a área de notificação no macOS, permitindo que os usuários acessem o aplicativo rapidamente a partir do canto superior direito da tela.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const { app, BrowserWindow, Menu, shell, clipboard } = require('electron');
+const { app, BrowserWindow, Menu, Tray, shell, clipboard, Notification } = require('electron');
 const path = require('path');
 
 let mainWindow;
+let appIcon = null;
 
 app.whenReady().then(() => {
   mainWindow = new BrowserWindow({
@@ -53,6 +54,13 @@ app.whenReady().then(() => {
               click: () => mainWindow.webContents.selectAll()
             }
           ]
+        },
+        { type: 'separator' },
+        {
+          label: 'Minimizar',
+          click: () => {
+            mainWindow.minimize();
+          }
         }
       ]
     }
@@ -63,19 +71,49 @@ app.whenReady().then(() => {
 
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
-  mainWindow.webContents.on('dom-ready', () => {
-    mainWindow.webContents.insertCSS(`
-
-    `);
-
-    mainWindow.webContents.executeJavaScript(`
-
-    `);
+  mainWindow.on('close', (event) => {
+    if (!app.isQuitting) {
+      event.preventDefault();
+      mainWindow.hide();
+    }
   });
+
+  appIcon = new Tray(path.join(__dirname, 'images/icon-AI.png'));
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Abrir Aplicativo',
+      click: () => {
+        mainWindow.show();
+      }
+    },
+    {
+      label: 'Sair',
+      click: () => {
+        app.isQuitting = true;
+        app.quit();
+      }
+    }
+  ]);
+  appIcon.setToolTip('Meu App Tray');
+  appIcon.setContextMenu(contextMenu);
+
+
+  setTimeout(() => {
+    new Notification({
+      title: 'Notificação',
+      body: 'Olá! Esta é uma notificação do seu aplicativo.'
+    }).show();
+  }, 5000); 
 });
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
   }
 });
